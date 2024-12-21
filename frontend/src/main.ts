12fd25c095cf072e0a3bd0a3a27dddd08b122566
Ryan <ryanbprog@gmail.com>
2024-12-21 22:08:07 +0000

    navigation logic added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,7 @@ import Home from "./pages/home.ts";
 import Projects from "./pages/projects.ts";
 
 document.getElementById("header-cont")!.innerHTML = Header();
-// document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = Projects();
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
 
 const header = document.getElementById("header");
 const mobileButton = document.getElementById("mobile-nav-btn");
@@ -24,5 +23,22 @@ navButtons.forEach((button) => {
   button.addEventListener("click", () => {
     navButtons.forEach((btn) => btn.classList.remove("active"));
     button.classList.add("active");
+
+    switch (button.id) {
+      case "home-nav-button":
+        document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
+        break;
+      case "about-nav-button":
+        document.querySelector<HTMLDivElement>("#app")!.innerHTML =
+          "<section><h1>About Page</h1><p>About content goes here.</p></section>";
+        break;
+      case "portfolio-nav-button":
+        document.querySelector<HTMLDivElement>("#app")!.innerHTML = Projects();
+        break;
+      case "contact-nav-button":
+        document.querySelector<HTMLDivElement>("#app")!.innerHTML =
+          "<section><h1>Contact Page</h1><p>Contact content goes here.</p></section>";
+        break;
+    }
   });
 });
