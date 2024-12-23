175cbacf013940bb94ae088d4aedc5d166497d80
Ryan <ryanbprog@gmail.com>
2024-12-23 18:32:38 +0000

    change home page to be standard html instead of js components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,44 +1,70 @@
 import "./style.css";
-import Header from "./components/header.ts";
-import Home from "./pages/home.ts";
-import Projects from "./pages/projects.ts";
-import About from "./pages/about.ts";
-import Contact from "./pages/contact.ts";
+import { gsap } from "gsap";
+// import Header from "./components/header.ts";
+// import Home from "./pages/home.ts";
+// import Projects from "./pages/projects.ts";
+// import About from "./pages/about.ts";
+// import Contact from "./pages/contact.ts";
 
-document.getElementById("header-cont")!.innerHTML = Header();
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
+const mainHeading = document.getElementsByClassName("main-heading");
 
-const header = document.getElementById("header");
-const mobileButton = document.getElementById("mobile-nav-btn");
-
-mobileButton?.addEventListener("click", () => {
-  if (header?.classList.contains("mobile-nav-open")) {
-    header?.classList.remove("mobile-nav-open");
-  } else {
-    header?.classList.add("mobile-nav-open");
+gsap.fromTo(
+  mainHeading,
+  {
+    y: -100,
+    // opacity: 0,
+  },
+  {
+    y: 0,
+    // opacity: 1,
+    duration: 2,
+    stagger: 0.5,
   }
-});
+);
 
-const navButtons = document.querySelectorAll("[id$='nav-button']");
-
-navButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    navButtons.forEach((btn) => btn.classList.remove("active"));
-    button.classList.add("active");
-
-    switch (button.id) {
-      case "home-nav-button":
-        document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
-        break;
-      case "about-nav-button":
-        document.querySelector<HTMLDivElement>("#app")!.innerHTML = About();
-        break;
-      case "portfolio-nav-button":
-        document.querySelector<HTMLDivElement>("#app")!.innerHTML = Projects();
-        break;
-      case "contact-nav-button":
-        document.querySelector<HTMLDivElement>("#app")!.innerHTML = Contact();
-        break;
-    }
+document.querySelectorAll(".more-button").forEach((button) => {
+  gsap.to(button, {
+    y: -8,
+    repeat: -1,
+    yoyo: true,
+    duration: 0.6,
   });
 });
+
+// document.getElementById("header-cont")!.innerHTML = Header();
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
+
+// const header = document.getElementById("header");
+// const mobileButton = document.getElementById("mobile-nav-btn");
+
+// mobileButton?.addEventListener("click", () => {
+//   if (header?.classList.contains("mobile-nav-open")) {
+//     header?.classList.remove("mobile-nav-open");
+//   } else {
+//     header?.classList.add("mobile-nav-open");
+//   }
+// });
+
+// const navButtons = document.querySelectorAll("[id$='nav-button']");
+
+// navButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     navButtons.forEach((btn) => btn.classList.remove("active"));
+//     button.classList.add("active");
+
+//     switch (button.id) {
+//       case "home-nav-button":
+//         document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
+//         break;
+//       case "about-nav-button":
+//         document.querySelector<HTMLDivElement>("#app")!.innerHTML = About();
+//         break;
+//       case "portfolio-nav-button":
+//         document.querySelector<HTMLDivElement>("#app")!.innerHTML = Projects();
+//         break;
+//       case "contact-nav-button":
+//         document.querySelector<HTMLDivElement>("#app")!.innerHTML = Contact();
+//         break;
+//     }
+//   });
+// });
