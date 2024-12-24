f854348d7177b50cbb137ccb1aeffc3bfe7afe9e
Ryan <ryanbprog@gmail.com>
2024-12-24 09:16:08 +0000

    added lenis smooth scrolling

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,26 @@
 import "./style.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "lenis";
 
+// SMOOTH SCROLLING
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+gsap.ticker.lagSmoothing(0);
+
+requestAnimationFrame(raf);
+
+// GSAP ANIMATIONS
 gsap.registerPlugin(ScrollTrigger);
 
 const mainHeading = document.querySelectorAll(".main-heading");
