c663b164569d2dd4ffc9be9d11d1ce909518e4eb
Ryan <ryanbprog@gmail.com>
2024-12-26 14:40:22 +0000

    template pages created

---
+++ astro/src/data/projects.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "Better Spanish",
+    slug: "better-spanish",
+    description: "A language learning platform for Spanish.",
+    image: "/images/better-spanish.png",
+  },
+  {
+    title: "My Portfolio",
+    slug: "my-portfolio",
+    description: "A showcase of my work as a developer.",
+    image: "/images/my-portfolio.png",
+  },
+];
