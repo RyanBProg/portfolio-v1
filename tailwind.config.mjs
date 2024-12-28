1081b3b9ddd7605912f3ba08f3edce9c4ef6ea83
Ryan <ryanbprog@gmail.com>
2024-12-28 14:34:55 +0000

    added local font to project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        "space-grotesk": ['"Space Grotesk"', "sans-serif"],
+        sans: ["SpaceGrotesk", ...defaultTheme.fontFamily.sans],
       },
     },
   },
