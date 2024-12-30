2eb24d816e9634d9bd0e79282b1096f498592020
Ryan <ryanbprog@gmail.com>
2024-12-30 15:31:56 +0000

    created badges data to render them out conditionally

---
+++ src/content/techBadges.ts
@@ -0,0 +1,31 @@
+import typescriptIcon from "../assets/icons/typescript.svg";
+import javascriptIcon from "../assets/icons/javascript.svg";
+import reactIcon from "../assets/icons/react.svg";
+import nextJSIcon from "../assets/icons/next-js.svg";
+import astroIcon from "../assets/icons/astro.svg";
+import nodeJSIcon from "../assets/icons/node-js.svg";
+import expressJSIcon from "../assets/icons/express.svg";
+import tailwindIcon from "../assets/icons/tailwind.svg";
+import mongoDBIcon from "../assets/icons/mongo-db.svg";
+import bootstrapIcon from "../assets/icons/bootstrap.svg";
+import htmlIcon from "../assets/icons/html.svg";
+import cssIcon from "../assets/icons/css.svg";
+import gitIcon from "../assets/icons/git.svg";
+import githubIcon from "../assets/icons/github-round.svg";
+
+export default [
+  { title: "TypeScript", alt: "typescript", img: typescriptIcon },
+  { title: "JavaScript", alt: "javascript", img: javascriptIcon },
+  { title: "React", alt: "react", img: reactIcon },
+  { title: "Next.js", alt: "next.js", img: nextJSIcon },
+  { title: "Astro", alt: "astro", img: astroIcon },
+  { title: "Node.js", alt: "node.js", img: nodeJSIcon },
+  { title: "Express.js", alt: "express.js", img: expressJSIcon },
+  { title: "Tailwind", alt: "tailwind", img: tailwindIcon },
+  { title: "MongoDB", alt: "mongo db", img: mongoDBIcon },
+  { title: "Bootstrap", alt: "bootstrap", img: bootstrapIcon },
+  { title: "HTML", alt: "html", img: htmlIcon },
+  { title: "CSS", alt: "css", img: cssIcon },
+  { title: "Git", alt: "git", img: gitIcon },
+  { title: "GitHub", alt: "github", img: githubIcon },
+];
