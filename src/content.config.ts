5f849d97c99633686bf1d13a871d5ef2f3b03995
Ryan <ryanbprog@gmail.com>
2025-01-01 19:40:45 +0000

    corrected image in schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,14 +3,16 @@ import { glob } from "astro/loaders";
 
 const projectsCollection = defineCollection({
   loader: glob({ pattern: "*.md", base: "./src/collections/projects/" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    techStack: z.array(z.string()),
-    projectUrl: z.string(),
-    githubUrl: z.string(),
-    slug: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      techStack: z.array(z.string()),
+      projectUrl: z.string(),
+      githubUrl: z.string(),
+      slug: z.string(),
+      image: image(),
+    }),
 });
 
 export const collections = { projectsCollection };
